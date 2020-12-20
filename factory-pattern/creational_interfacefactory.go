f7e98f075acd2b5dff78520eda0e9cb55017de2e
shashank.g172 <shashank.g172@gmail.com>
2020-12-20 09:38:54 -0500

    Complete full example

--- factory-pattern/creational_interfacefactory.go
+++ factory-pattern/creational_interfacefactory.go
@@ -6,6 +6,11 @@ type Person interface {
 	SayHello()
 }
 
+type tiredPerson struct {
+	name string
+	age  int
+}
+
 type person struct {
 	name string
 	age  int
@@ -15,7 +20,16 @@ func (p *person) SayHello() {
 	fmt.Printf("Hi, my name is %s and I am %d years old\n", p.name, p.age)
 }
 
+func (t *tiredPerson) SayHello() {
+	fmt.Println("Can't talk, too tired")
+}
+
 func NewPerson(name string, age int) Person {
+	// Hide the logic of what person is being created underneath the interface instantiation, instead
+	// of giving a user control of it
+	if age > 80 {
+		return &tiredPerson{name, age}
+	}
 	return &person{name, age}
 }
 
