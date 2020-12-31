d0742cdbdccad82d1708ad4bb19cbded5646b1ed
shashank.g172 <shashank.g172@gmail.com>
2020-12-31 09:13:06 -0500

    Updating the template pattern example

---
+++ template-pattern/functional_templatemethod.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func PlayGame(start, takeTurn func(),
+	haveWinner func() bool,
+	winningPlayer func() int) {
+	start()
+	for !haveWinner() {
+		takeTurn()
+	}
+	fmt.Printf("Player %d wins.\n", winningPlayer())
+}
+
+// No interfaces or structs, operates purely on methods
+func main() {
+	turn, maxTurns, currentPlayer := 1, 10, 0
+
+	start := func() {
+		fmt.Println("Starting a game of chess.")
+	}
+
+	takeTurn := func() {
+		turn++
+		fmt.Printf("Turn %d taken by player %d\n",
+			turn, currentPlayer)
+		currentPlayer = (currentPlayer + 1) % 2
+	}
+
+	haveWinner := func() bool {
+		return turn == maxTurns
+	}
+
+	winningPlayer := func() int {
+		return currentPlayer
+	}
+
+	PlayGame(start, takeTurn, haveWinner, winningPlayer)
+}
